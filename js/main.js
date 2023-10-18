05708006dc1c4224bdaf812f850246a41d357a59
noiladev <noila10122009@gmail.com>
2023-10-18 13:36:15 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+for (var i = 1; i <= 100; i++)  {
+    if (i % 3 == 0  && i % 7 == 0) {
+        console.log(i);
+    }
+}
+
+console.log('Teskari  tartib');
+
+for (var i = 100; i >= 1;  i--)  {
+    if (i % 3 == 0 && i % 7 == 0)  {
+        console.log(i);
+    }
+}
